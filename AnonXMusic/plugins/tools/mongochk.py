1bc10dac5fabed3759dad68a524d4ca1cf7553f7
DeepVikky88 <radheymusic51@gmail.com>
2025-07-26 21:00:03 +0530

    Update mongochk.py

--- AnonXMusic/plugins/tools/mongochk.py
+++ AnonXMusic/plugins/tools/mongochk.py
@@ -3,7 +3,7 @@ import re
 from pymongo import MongoClient
 from pyrogram import filters
 from pyrogram.types import Message
-from SystemMusic import app
+from AnonXMusic import app
 
 
 mongo_url_pattern = re.compile(r"mongodb(?:\+srv)?:\/\/[^\s]+")
